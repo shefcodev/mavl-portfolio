a61389e205f8995fc33fd523ab208fc77fca73f9
USER <user@USERs-MacBook-Pro.local>
2025-07-14 06:11:49 -0700

    rr

--- constants/index.ts
+++ constants/index.ts
@@ -100,16 +100,6 @@ export const projects = {
       id: "port5",
       title: "Detail",
       description:
-        "Managed and executed Facebook content strategy across 18 BNI chapters, aligning branding and engagement. Developed campaigns that promoted local chapter events, spotlighted members, and drove consistent growth across the board.",
-      url: "https://havco-bank.vercel.app",
-      gitLink: "https://github.com/shefcodev/havco_banking_app.git",
-      src: "/images/port1.png",
-      stacks: [""],
-    },
-    {
-      id: "port6",
-      title: "Detail",
-      description:
         "Coordinated, scheduled, and managed calendars for meetings and events across all 18 chapters. Ensured smooth communication flows between directors, leadership teams, and members.",
       url: "https://nike-clone-app-lake.vercel.app/",
       gitLink: "https://github.com/shefcodev/nike-clone-app.git",
