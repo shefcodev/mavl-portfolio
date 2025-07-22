57474eaac6af325ca455cc88464a69c45c856f3d
USER <user@USERs-MacBook-Pro.local>
2025-07-22 13:40:03 -0700

    ff

--- constants/index.ts
+++ constants/index.ts
@@ -16,7 +16,7 @@ export const details = {
   aboutDesc: [
     "I help businesses grow smarter by merging administrative expertise with automation and data-driven insights—streamlining operations, cutting inefficiencies, and unlocking opportunities.",
     " Passionate about collaborating with ethical, growth-minded entrepreneurs, I thrive on using referral marketing strategies to forge powerful partnerships. Let’s connect and build something remarkable!",
-    "  When I’m not optimizing workflows: ⚽ Football enthusiast | 🎷 Jazz lover | 🎌 Anime fan"
+    "  When I’m not optimizing workflows: Football enthusiast | Afrobeats, Amapiano and Jazz lover |  Anime fan"
   ]
 };
 // My expertise lies in crafting clean, efficient code that not only meets but exceeds user expectations.
