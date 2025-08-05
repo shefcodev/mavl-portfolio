325494cbe2ae292819ee16c868d352379f51ac82
USER <user@USERs-MacBook-Pro.local>
2025-08-05 16:44:18 -0700

    dd

--- components/About.tsx
+++ components/About.tsx
@@ -40,8 +40,8 @@ const About = () => {
 
           <motion.div {...motionValues}>
             {/* {aboutDesc.map((desc, idx) => <li key={idx}><p className="about-desc">{desc}</p></li>)} */}
-            <p className="about-desc mb-2"><span className="font-bold">Meet Marvellous</span> — your strategic partner in operations, technology, and growth.</p>
-            <p className="about-desc mb-2">With a powerful blend of administrative expertise, tech-savviness, and data-driven insight, I help ethical, growth-minded entrepreneurs streamline operations, eliminate inefficiencies, and unlock hidden opportunities. As a Virtual Assistant and Online Business Partner, my mission is to help lean teams run like enterprise-grade machines.</p>
+            <p className="about-desc mb-2"><span className="font-bold">Marvellous</span> — your strategic partner in operations, technology, and growth.</p>
+            <p className="about-desc mb-2">With a powerful blend of administrative expertise, tech-savviness, and data-driven insight, I help ethical, growth-minded entrepreneurs streamline operations, eliminate inefficiencies, and unlock hidden opportunities. my mission is to help lean teams run like enterprise-grade machines.</p>
             <p className="about-desc mb-2">I go far beyond traditional admin work—integrating <span className="font-bold">automation, IT support, customer service,</span> and <span className="font-bold">creative problem-solving</span> into every aspect of your business. My diverse skill set spans:
             </p>
             <ul className="mb-2 list">
